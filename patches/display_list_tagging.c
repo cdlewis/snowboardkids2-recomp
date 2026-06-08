4c999ff67c34011e6aa1f39019c8739afb54c750
Chris Lewis <me@chrislewis.au>
2026-06-08 12:24:25 -0700

    Tag shadows
    Co-authored-by: Darío <dariosamo@gmail.com>

--- patches/display_list_tagging.c
+++ patches/display_list_tagging.c
@@ -2,6 +2,8 @@
 
 extern Gfx* gDisplayListAllocPtr;
 extern ActiveViewportState* gActiveViewport;
+extern s16 gGraphicsMode;
+extern Gfx gPlayerShadowRenderSetupDl[];
 
 extern void prepareDisplayListRenderState(DisplayListObject*);
 extern void setupDisplayListMatrix(DisplayListObject*);
@@ -426,5 +428,99 @@ RECOMP_PATCH void renderBillboardedOverlayDisplayList(DisplayListObject* arg0) {
 
         // @recomp Pop the matrix group.
         popObjectMatrixGroup();
+    }
+}
+
+// @recomp Modified to insert a matrix group around the shadow.
+RECOMP_PATCH void renderRacerProjectedShadow(Player* player) {
+    OutputStruct_19E80 shadowTexture;
+    s32 shadowAlpha;
+    s32 sampleIndex;
+
+    if (player->shadowMeshNeedsUpdate != 0) {
+        player->shadowVertices = arenaAlloc16(0x90);
+        if (player->shadowVertices != NULL) {
+            sampleIndex = 0;
+            shadowAlpha = 0x50;
+            do {
+                s32 vertexByteOffset = sampleIndex << 4;
+                *(s16*) (vertexByteOffset + (s32) player->shadowVertices) =
+                    (s16) ((*(volatile s32*) &player->shadowSamplePositions[sampleIndex].x -
+                            player->shadowSamplePositions[0].x) >>
+                           14);
+                *(s16*) (vertexByteOffset + (s32) player->shadowVertices + 2) =
+                    (s16) ((*(volatile s32*) &player->shadowSamplePositions[sampleIndex].y -
+                            player->shadowSamplePositions[0].y) >>
+                           14);
+                *(s16*) (vertexByteOffset + (s32) player->shadowVertices + 4) =
+                    (s16) ((*(volatile s32*) &player->shadowSamplePositions[sampleIndex].z -
+                            player->shadowSamplePositions[0].z) >>
+                           14);
+                *(u8*) (vertexByteOffset + (s32) player->shadowVertices + 0xC) = 0;
+                *(u8*) (vertexByteOffset + (s32) player->shadowVertices + 0xD) = 0;
+                *(u8*) (vertexByteOffset + (s32) player->shadowVertices + 0xE) = 0;
+                *(u8*) (vertexByteOffset + (s32) player->shadowVertices + 0xF) = shadowAlpha;
+                sampleIndex++;
+            } while (sampleIndex < 9);
+
+            player->shadowVertices[0].v.tc[0] = -0x20;
+            player->shadowVertices[0].v.tc[1] = -0x20;
+            player->shadowVertices[1].v.tc[0] = 0x3E0;
+            player->shadowVertices[1].v.tc[1] = -0x20;
+            player->shadowVertices[2].v.tc[0] = 0x7E0;
+            player->shadowVertices[2].v.tc[1] = -0x20;
+            player->shadowVertices[3].v.tc[0] = -0x20;
+            player->shadowVertices[3].v.tc[1] = 0x3E0;
+            player->shadowVertices[4].v.tc[0] = 0x3E0;
+            player->shadowVertices[4].v.tc[1] = 0x3E0;
+            player->shadowVertices[5].v.tc[0] = 0x7E0;
+            player->shadowVertices[5].v.tc[1] = 0x3E0;
+            player->shadowVertices[6].v.tc[0] = -0x20;
+            player->shadowVertices[6].v.tc[1] = 0x7E0;
+            player->shadowVertices[7].v.tc[0] = 0x3E0;
+            player->shadowVertices[7].v.tc[1] = 0x7E0;
+            player->shadowVertices[8].v.tc[0] = 0x7E0;
+            player->shadowVertices[8].v.tc[1] = 0x7E0;
+        }
+
+        player->shadowMatrix = arenaAlloc16(0x40);
+        if (player->shadowMatrix != NULL) {
+            memcpy(&gScaleMatrix.translation, &player->shadowSamplePositions[0], sizeof(Vec3i));
+            transform3DToN64Mtx(&gScaleMatrix, player->shadowMatrix);
+        }
+        player->shadowMeshNeedsUpdate = 0;
+    }
+
+    if (player->shadowVertices != NULL && player->shadowMatrix != NULL) {
+        // @recomp Push the matrix group for this player. Use the address of the player's shadow matrix pointer as its ID.
+        // TODO: This can be replaced with a better ID system by extending Player data as well.
+        gEXMatrixGroupSimple(gDisplayListAllocPtr++, (u32) (&player->shadowMatrix), G_EX_PUSH, G_MTX_MODELVIEW,
+                             G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_SKIP,
+                             G_EX_COMPONENT_INTERPOLATE, G_EX_COMPONENT_AUTO, G_EX_ORDER_LINEAR, G_EX_EDIT_NONE,
+                             G_EX_COMPONENT_AUTO, G_EX_COMPONENT_AUTO);
+
+        gSPMatrix(gDisplayListAllocPtr++, player->shadowMatrix, (G_MTX_NOPUSH | G_MTX_LOAD) | G_MTX_MODELVIEW);
+        gGraphicsMode = -1;
+        gSPDisplayList(gDisplayListAllocPtr++, gPlayerShadowRenderSetupDl);
+        getTableEntryByU16Index(player->assetTable, 0, &shadowTexture);
+
+        gDPSetTextureImage(gDisplayListAllocPtr++, G_IM_FMT_I, G_IM_SIZ_16b, 1, shadowTexture.data_ptr);
+        gDPSetTile(gDisplayListAllocPtr++, G_IM_FMT_I, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, G_TX_CLAMP, 5, G_TX_NOLOD,
+                   G_TX_CLAMP, 5, G_TX_NOLOD);
+        gDPLoadSync(gDisplayListAllocPtr++);
+        gDPLoadBlock(gDisplayListAllocPtr++, G_TX_LOADTILE, 0, 0, 255, 1024);
+        gDPPipeSync(gDisplayListAllocPtr++);
+        gDPSetTile(gDisplayListAllocPtr++, G_IM_FMT_I, G_IM_SIZ_4b, 2, 0, G_TX_RENDERTILE, 0, G_TX_CLAMP, 5, G_TX_NOLOD,
+                   G_TX_CLAMP, 5, G_TX_NOLOD);
+        gDPSetTileSize(gDisplayListAllocPtr++, G_TX_RENDERTILE, 0, 0, 31 << 2, 31 << 2);
+
+        gSPVertex(gDisplayListAllocPtr++, player->shadowVertices, 9, 0);
+        gSP2Triangles(gDisplayListAllocPtr++, 0, 1, 3, 0, 1, 4, 3, 0);
+        gSP2Triangles(gDisplayListAllocPtr++, 1, 2, 5, 0, 5, 4, 1, 0);
+        gSP2Triangles(gDisplayListAllocPtr++, 3, 4, 7, 0, 7, 6, 3, 0);
+        gSP2Triangles(gDisplayListAllocPtr++, 4, 5, 7, 0, 5, 8, 7, 0);
+
+        // @recomp Pop the matrix group.
+        gEXPopMatrixGroup(gDisplayListAllocPtr++, G_MTX_MODELVIEW);
     }
 }
