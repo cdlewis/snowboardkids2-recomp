f0098b75d895b39ec41c6428c73a4e7c7dd6b0a0
Chris Lewis <me@chrislewis.au>
2026-06-21 07:27:34 -0700

    bump version to v1.0.4

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -52,7 +52,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "1.0.3";
+const std::string version_string = "1.0.4";
 
 template <typename... Ts> void exit_error(const char* str, Ts... args) {
     // TODO pop up an error
